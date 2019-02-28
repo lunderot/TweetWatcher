8f01019c842e621bf476d3c5f079b112e2c37621
lunderot <matriz@live.se>
2019-02-28 14:53:21 +0100

    Basic twitter streaming

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-from twitter import *
+
+from twython import Twython
+from twython import TwythonStreamer
+import json
+
+with open("auth.json", "r") as read_file:
+    auth = json.load(read_file)
+
+class MyStreamer(TwythonStreamer):
+    def on_success(self, data):
+        if 'text' in data:
+            print(data['text'].encode('utf-8'))
+        # Want to disconnect after the first result?
+        # self.disconnect()
+
+    def on_error(self, status_code, data):
+        print(status_code, data)
+
 
 
 def main():
-	print("Hello")
+	# Requires Authentication as of Twitter API v1.1
+	stream = MyStreamer(auth['APP_KEY'], auth['APP_SECRET'],
+						auth['OAUTH_TOKEN'], auth['OAUTH_TOKEN_SECRET'])
+
+	stream.statuses.filter(track='twitter')
+	# stream.user()
+	# Read the authenticated users home timeline
+	# (what they see on Twitter) in real-time
+	# stream.site(follow='twitter')
 
 if __name__ == "__main__":
 	main()
